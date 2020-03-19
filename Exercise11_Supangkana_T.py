47bb9c7dd05ef59b44d804fa203b8c12f42c49ec
SupangkanaT/ <44719754+SupangkanaT@users.noreply.github.com>
2020-03-19 20:02:39 +0700

    Add exercise11

---
+++ Exercise11_Supangkana_T.py
@@ -0,0 +1,11 @@
+inputNumber = int(input("Number : "))
+for i in range(inputNumber):
+    star = ((2*i)+1)*"*"
+    print(" "*(inputNumber-i) + star)
+
+
+'''
+number2 = int(input("Number2"))
+for x in range(numbe1r2):
+    print("*"*(x+1))
+'''
