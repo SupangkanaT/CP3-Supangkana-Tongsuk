54bfc81becd16a412b31c5ce09681d47fb90be16
SupangkanaT/ <44719754+SupangkanaT@users.noreply.github.com>
2020-03-19 20:49:01 +0700

    Add Lecture50

---
+++ Lecture50_Supangkana_T.py
@@ -0,0 +1,18 @@
+def addNumber(x, y):
+    print(x+y)
+addNumber(10,10)
+
+
+def minusNumber(x, y):
+    print(x-y)
+minusNumber(100,20)
+
+
+def multipliedNumber(x, y):
+    print(x*y)
+multipliedNumber(10,10)
+
+
+def divideNumber(x, y):
+    print(x/y)
+divideNumber(100,10)
